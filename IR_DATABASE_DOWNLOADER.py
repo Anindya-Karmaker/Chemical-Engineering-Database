1af466e1e45d456ffcdae23723ccc0ca03cd741c
Anindya-Karmaker <71129551+Anindya-Karmaker@users.noreply.github.com>
2020-09-13 20:41:08 +0600

    V1.1

--- IR_DATABASE_DOWNLOADER.py
+++ IR_DATABASE_DOWNLOADER.py
@@ -10,24 +10,28 @@ from tkinter.scrolledtext import ScrolledText
 import string
 conn=sqlite3.connect('IR_DATA.sqlite')
 cur=conn.cursor()   
-fg=[]#FUNCTIONAL GROUP STORAGE 
-vib=['all','stretching', 'bending']#TYPE OF VIBRATION
-vib_type='all'
-func_type='all'
+ir_fg=[]#FUNCTIONAL GROUP STORAGE 
+ir_vib=['all','stretching', 'bending']#TYPE OF VIBRATION
+ir_vib_type='all'
+ir_func_type='all'
 cm=0
 dat_check=0
 cmdlineq=''
-statement='ENTER INFORMATION'
+statement=''
 list_item=''
-def func_group():
-    global fg
-    cmdlineq="SELECT * FROM IR_DATA"
-    row=cur.fetchone()
-    for row in cur.execute(cmdlineq):
-           fg.append(str(row[6]))
-    fg = list(dict.fromkeys(fg))
-    fg.sort()
-    fg.insert(0,'all')
+def ir_func_group():
+    global ir_fg
+    global statement
+    try:
+        cmdlineq="SELECT * FROM IR_DATA"
+        row=cur.fetchone()
+        for row in cur.execute(cmdlineq):
+               ir_fg.append(str(row[6]))
+        ir_fg = list(dict.fromkeys(ir_fg))
+        ir_fg.sort()
+        ir_fg.insert(0,'all')
+    except:
+        statement="NO DATABASE FOUND PLEASE UPDATE DATABASE!"
 def update_ir_db():
     ctx=ssl.create_default_context()
     ctx.check_hostname=False
@@ -95,14 +99,14 @@ def update_ir_db():
                         command3="INSERT INTO " +"IR_DATA"+" (wavenumber_upper, wavenumber_lower,intensity, peak, functional_group,type_of_vibration, group_name,other) VALUES(?,?,?,?,?,?,?,?)"
                         cur.execute(command3, (wavenumber_upper,wavenumber_lower,intensity,peak,functional_group,type_of_vibration,group_name,other))
         conn.commit()
-    messagebox.showinfo("STATUS", "DATABASE UPDATED SUCCESSFULLY") 
-
+    ir_func_group()
 
     
 def interface_user():
+    global statement
     global cm
     window = Tk()
-    window.geometry("860x350")
+    window.geometry("870x350")
     window.title("CHEMICAL ENGINEERING TOOLKIT")
     window.resizable(False, False)
     TC= ttk.Notebook(window)
@@ -115,7 +119,7 @@ def interface_user():
     IR_ESTIMATE.pack()
     infoview=Frame(INFO)
     infoview.pack()
-    lbl5 = Label(infoview, text="CHEMICAL ENGINEERING TOOLKIT", font=("Arial Bold", 14))
+    lbl5 = Label(infoview, text="CHEMICAL ENGINEERING TOOLKIT V1.1", font=("Arial Bold", 14))
     lbl5.pack(fill=X)
     lbl6 = Label(infoview, text="Software Created by Anindya Karmker", font=("Arial Bold", 12))
     lbl6.pack(fill=X)
@@ -126,63 +130,68 @@ def interface_user():
     lble1.config(width=860,height=2)
     lble1.config(state='disabled')
     lble1.pack(fill=X)
-    def help_me():
-        messagebox.showinfo("GUIDELINES", "Enter the infrared spectrum wavenumber. Then select Estimate Functional Group button to show the corresponding matching functional groups. You can also limit the chemical group and type of vibration by choosing the options from the listbox. Once in a year or two, you can update the database for any new compounds") 
-        # lbl4 = Label(infoview, text="Guidelines", font=("Arial Bold", 12))
-        # lbl4.pack(fill=X)
-        # info1 = Message(infoview, text = "Enter the infrared spectrum wavenumber. Then select Estimate Functional Group button to show the corresponding matching functional groups. You can also limit the chemical group and type of vibration by choosing the options from the listbox. Once in a year or two, you can update the database for any new compounds")
-        # info1.config(font=('Arial', 12), justify=CENTER, width=860)
-        # info1.pack(fill=X)
-    #lbupdate=Label(finalshowdown, text="DATABASE UPDATED", font=("Arial", 14)).pack(side=TOP, anchor=NW)
+
     enteroption = Frame(IR_ESTIMATE)
-    enteroption.grid(row=0,column=0, sticky=NW)
-    optlabel=Label(enteroption, text="IR wavenumber (per cm)", justify='center',font=("Arial", 13,"bold"))
-    optlabel.pack(side=TOP)
+    enteroption.grid(row=0,column=0, padx=5,pady=5,sticky=N)
+    optlabel=Label(enteroption, text="IR wavenumber (per cm)", justify='center',font=("Arial", 13,"bold")).pack(side=TOP)
     option=Entry(enteroption, width=25,font=("Arial", 12))
     option.pack(anchor=NW,fill="x")
     solve_ir=Button(enteroption, text="Estimate Functional Group", font=("Arial", 13,"bold"), height=1, width=23)
     solve_ir.pack(side=TOP,anchor=N,fill='x',expand=TRUE)
     updatedb=Button(enteroption, text="Update Database", font=("Arial", 13,"bold"), height=1, width=23)
     updatedb.pack(side=TOP,anchor=N,fill='x',expand=TRUE)
-    updatedb.configure(command=update_ir_db)
     help1=Button(enteroption, text="Help", font=("Arial", 13,"bold"), height=1, width=23)
     help1.pack(side=TOP,anchor=N,fill='x',expand=TRUE)
-    help1.configure(command=help_me)
     typevib=Frame(IR_ESTIMATE)
-    typevib.grid(row=0,column=2,sticky=NW)
+    typevib.grid(row=0,column=2,padx=5,pady=5,sticky=N)
     lb1=Label(typevib, text="Choose Type of Vibration", justify='center',font=("Arial", 13,"bold")).pack(side=TOP)
     listvib = Listbox(typevib, width=23, height=8, justify='center',font=("Helvetica", 12),selectmode=BROWSE,exportselection=False)
     listvib.pack(side=LEFT, fill="x")
-    typefg=Frame(IR_ESTIMATE)
-    typefg.grid(row=0,column=1)
-    lb1=Label(typefg, text="Choose Chemical Group",justify='center', font=("Arial", 13,"bold")).pack(side=TOP)
-    option2=Entry(typefg, width=23,font=("Arial", 12))
+    typeir_fg=Frame(IR_ESTIMATE)
+    typeir_fg.grid(row=0,column=1,padx=5,pady=5,sticky=N)
+    lb1=Label(typeir_fg, text="Choose Chemical Group",justify='center', font=("Arial", 13,"bold")).pack(side=TOP)
+    option2=Entry(typeir_fg, width=23,font=("Arial", 12))
     option2.pack(side=TOP, anchor=NW,fill="x")
-    search=Button(typefg, text="Search", font=("Arial", 13,"bold"), height=1, width=23)
+    search=Button(typeir_fg, text="Search", font=("Arial", 13,"bold"), height=1, width=23)
     search.pack(side=TOP,anchor=N,fill='x',expand=TRUE)
     
-    listfunc = Listbox(typefg, width=40, height=5,justify='center', font=("Helvetica", 12),selectmode=BROWSE,exportselection=False)
+    listfunc = Listbox(typeir_fg, width=40, height=5,justify='center', font=("Helvetica", 12),selectmode=BROWSE,exportselection=False)
     listfunc.pack(side=LEFT, fill="x")
-    scrollbar = Scrollbar(typefg, orient="vertical")
+    scrollbar = Scrollbar(typeir_fg, orient="vertical")
     scrollbar.config(command=listfunc.yview)
     scrollbar.pack(side=RIGHT,anchor=N, fill="y")
-    lbdisp=ScrolledText(ESTIMATE,width=300, height=10,font=("Arial", 12))
+    lbdisp=ScrolledText(ESTIMATE,width=300, height=10,padx=5,pady=5,font=("Arial", 12))
     lbdisp.pack(side=LEFT,fill='none')      
     lbdisp.configure(state='disabled')
     while cm!=1:
-        func_group() 
-        for items in range(len(vib)):
-            listvib.insert(items, vib[items])
-        for items in range(len(fg)):
-            listfunc.insert(items, fg[items])
-        cm=1   
-      
-    def vib_type_choosen(evt):
-        global vib_type
-        vib_type=str((listvib.get(ANCHOR)))
-    def func_type_choosen(evt):
-        global func_type
-        func_type=str((listfunc.get(ANCHOR)))
+        ir_func_group() 
+        listfunc.delete('0','end')
+        for items in range(len(ir_vib)):
+            listvib.insert(items, ir_vib[items])
+        for items in range(len(ir_fg)):
+            listfunc.insert(items, ir_fg[items])
+        lbdisp.configure(state='normal')
+        lbdisp.delete('1.0', END)
+        lbdisp.insert(INSERT,statement)
+        lbdisp.configure(state='disabled')
+        cm=1 
+    def ir_update():
+        update_ir_db()
+        lbdisp.configure(state='normal')
+        listfunc.delete('0','end')
+        for items in range(len(ir_fg)):
+            listfunc.insert(items, ir_fg[items])
+        statement="---------------DATABASE STATUS---------------\nDATABASE UPDATED SUCCESSFULLY\n"+'-'*60
+        lbdisp.delete('1.0', END)
+        lbdisp.insert(INSERT,statement)
+        lbdisp.configure(state='disabled')
+        print(statement)
+    def ir_vib_type_choosen(evt):
+        global ir_vib_type
+        ir_vib_type=str((listvib.get(ANCHOR)))
+    def ir_func_type_choosen(evt):
+        global ir_func_type
+        ir_func_type=str((listfunc.get(ANCHOR)))
     def solvefunc():
         lbdisp.configure(state='normal')
         x=option.get()
@@ -190,19 +199,19 @@ def interface_user():
             x=0
         global statement
         global dat_check
-        global vib_type
-        global func_type
+        global ir_vib_type
+        global ir_func_type
         lbdisp.delete('1.0', END)
         global cmdlineq
         dat_check=0
-        if(vib_type=='all' and func_type=='all' or func_type==''):
+        if(ir_vib_type=='all' and ir_func_type=='all' or ir_func_type==''):
             cmdlineq="SELECT * FROM IR_DATA WHERE wavenumber_upper>="+str(x)+" AND wavenumber_lower<="+str(x)
-        elif(vib_type!='all' and func_type=='all'):
-            cmdlineq="SELECT * FROM IR_DATA WHERE type_of_vibration='"+str(vib_type)+"' AND wavenumber_upper>="+str(x)+" AND wavenumber_lower<="+str(x)
-        elif(vib_type=='all' and func_type!='all'):
-            cmdlineq="SELECT * FROM IR_DATA WHERE group_name='"+str(func_type)+"' AND wavenumber_upper>="+str(x)+" AND wavenumber_lower<="+str(x)
-        elif(vib_type!='all' and func_type!='all'):
-            cmdlineq="SELECT * FROM IR_DATA WHERE type_of_vibration='"+str(vib_type)+"' AND group_name='"+str(func_type)+"' AND wavenumber_upper>="+str(x)+" AND wavenumber_lower<="+str(x) 
+        elif(ir_vib_type!='all' and ir_func_type=='all'):
+            cmdlineq="SELECT * FROM IR_DATA WHERE type_of_vibration='"+str(ir_vib_type)+"' AND wavenumber_upper>="+str(x)+" AND wavenumber_lower<="+str(x)
+        elif(ir_vib_type=='all' and ir_func_type!='all'):
+            cmdlineq="SELECT * FROM IR_DATA WHERE group_name='"+str(ir_func_type)+"' AND wavenumber_upper>="+str(x)+" AND wavenumber_lower<="+str(x)
+        elif(ir_vib_type!='all' and ir_func_type!='all'):
+            cmdlineq="SELECT * FROM IR_DATA WHERE type_of_vibration='"+str(ir_vib_type)+"' AND group_name='"+str(ir_func_type)+"' AND wavenumber_upper>="+str(x)+" AND wavenumber_lower<="+str(x) 
         
         for row in cur.execute(cmdlineq):
             statement="---------------------------------------------------\n"+"WAVENUMBER RANGE: "+str(row[0])+"-"+str(row[1])+"\n"+"FUNCTIONAL GROUP: "+str(row[4])+"\n"+"TYPE OF VIBRATION: "+str(row[5]).upper()+"\n"+"PROBABLE CHEMICAL GROUP: "+str(row[6]).upper()+"\n"+"OTHER PROPERTY: "+str(row[7]).upper()+"\n"
@@ -217,21 +226,29 @@ def interface_user():
         lbdisp.insert(INSERT,statement)
         lbdisp.configure(state='disabled')
     solve_ir.configure(command=solvefunc)
-    def new_list_find(event):
+    def ir_new_list_find(event):
         listfunc.delete('0','end')
         global list_item
-        global fg
+        global ir_fg
         list_item=option2.get()
-        indices = [i for i, s in enumerate(fg) if list_item in s]
+        indices = [i for i, s in enumerate(ir_fg) if list_item in s]
         new_list=list()
         for i in indices:
-            new_list.append(fg[i])
+            new_list.append(ir_fg[i])
         for items in range(len(new_list)):
             listfunc.insert(items, new_list[items])
-    option2.bind('<Key-Return>',new_list_find)
-    listvib.bind('<<ListboxSelect>>',vib_type_choosen)
-    listfunc.bind('<<ListboxSelect>>',func_type_choosen)
-    search.bind('<Button-1>',new_list_find)
+    def ir_help_me():
+        lbdisp.configure(state='normal')
+        lbdisp.delete('1.0', END)
+        statement='-'*50+"HELP"+'-'*50+"\nEnter the infrared spectrum wavenumber. Then select Estimate Functional Group button to show the corresponding matching functional groups. You can also limit the chemical group and type of vibration by choosing the options from the listbox. Once in a year or two, you can update the database for any new compounds\n"+'-'*51+"END"+'-'*51
+        lbdisp.insert(INSERT,statement)
+        lbdisp.configure(state='disabled')
+    help1.configure(command=ir_help_me)
+    updatedb.configure(command=ir_update)
+    option2.bind('<Key-Return>',ir_new_list_find)
+    listvib.bind('<<ListboxSelect>>',ir_vib_type_choosen)
+    listfunc.bind('<<ListboxSelect>>',ir_func_type_choosen)
+    search.bind('<Button-1>',ir_new_list_find)
     window.mainloop()
 
 
